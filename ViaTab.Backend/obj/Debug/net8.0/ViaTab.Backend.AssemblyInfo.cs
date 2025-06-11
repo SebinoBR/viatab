96f701243c0d869310c2dae3ee8b5c0169ea9361
SebinoBR <sbrbartko@gmail.com>
2025-06-12 00:32:36 +0200

    Solution file changed and moved

--- ViaTab.Backend/obj/Debug/net8.0/ViaTab.Backend.AssemblyInfo.cs
+++ ViaTab.Backend/obj/Debug/net8.0/ViaTab.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViaTab.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a7fba3c089a2867a5ddfd010f6b2d0606aded2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViaTab.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViaTab.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
